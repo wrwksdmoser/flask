58960f8759a133d95a7a2f870c7cc058d333b456
wrwksdmoser <david.moser@wrwks.at>
2025-03-17 11:08:35 +0100

    test

--- testpipeline.groovy
+++ testpipeline.groovy
@@ -2,6 +2,11 @@ pipeline {
     agent any
 
     stages {
+        stage('Clone Repository') {
+            steps {
+                git url: 'https://github.com/wrwksdmoser/flask.git', credentialsId: '10cb4996-9f88-4019-85f7-d3ff7fa8a99b'
+            }
+        }
         stage('Hello') {
             steps {
                 echo 'Hello World'
